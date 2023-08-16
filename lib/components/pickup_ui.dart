ec258e608ecf7bf0509028c447f0346a1c3a2ffb
Deepak16080 <dy16080@gmail.com>
2023-08-16 11:59:31 +0530

    fix: fix mobile screen

--- lib/components/pickup_ui.dart
+++ lib/components/pickup_ui.dart
@@ -49,88 +49,91 @@ class _PickUpUiState extends State<PickUpUi> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 200),
+                  padding: const EdgeInsets.only(top: 184),
                   child: Container(
                     height: 300,
                     decoration: const BoxDecoration(
                         borderRadius: BorderRadius.vertical(top: Radius.circular(25)), color: Color(0xffE2F0F3)),
                     child: Column(
                       children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 40),
-                              child: CircleAvatar(
-                                radius: 30,
-                                backgroundColor: Colors.white,
-                                child: IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.dialpad,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 6),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 40),
+                                child: CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.white,
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.dialpad,
+                                    ),
+                                    alignment: Alignment.center,
+                                    iconSize: 30,
+                                    color: Colors.black,
                                   ),
-                                  alignment: Alignment.center,
-                                  iconSize: 30,
-                                  color: Colors.black,
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-                              child: CircleAvatar(
-                                radius: 30,
-                                backgroundColor: Colors.white,
-                                child: IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.mic_off_outlined,
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                                child: CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.white,
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.mic_off_outlined,
+                                    ),
+                                    alignment: Alignment.center,
+                                    iconSize: 30,
+                                    color: Colors.black,
                                   ),
-                                  alignment: Alignment.center,
-                                  iconSize: 30,
-                                  color: Colors.black,
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-                              child: CircleAvatar(
-                                radius: 30,
-                                backgroundColor: Colors.white,
-                                child: IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.volume_up_outlined,
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                                child: CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.white,
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.volume_up_outlined,
+                                    ),
+                                    alignment: Alignment.center,
+                                    iconSize: 30,
+                                    color: Colors.black,
                                   ),
-                                  alignment: Alignment.center,
-                                  iconSize: 30,
-                                  color: Colors.black,
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-                              child: CircleAvatar(
-                                radius: 30,
-                                backgroundColor: Colors.white,
-                                child: IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.more_vert_outlined,
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                                child: CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.white,
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.more_vert_outlined,
+                                    ),
+                                    alignment: Alignment.center,
+                                    iconSize: 30,
+                                    color: Colors.black,
                                   ),
-                                  alignment: Alignment.center,
-                                  iconSize: 30,
-                                  color: Colors.black,
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                         Padding(
-                          padding: EdgeInsets.only(bottom: 10),
+                          padding: EdgeInsets.symmetric(horizontal: 10),
                           child: Row(
                             children: const [
                               Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 24),
+                                padding: EdgeInsets.symmetric(horizontal: 10),
                                 child: Text(
                                   'Dialpad',
                                   style: TextStyle(fontSize: 16),
@@ -153,7 +156,7 @@ class _PickUpUiState extends State<PickUpUi> {
                                 ),
                               ),
                               Padding(
-                                padding: EdgeInsets.only(left: 50),
+                                padding: EdgeInsets.only(left: 55),
                                 child: Text(
                                   'more',
                                   style: TextStyle(fontSize: 16),
