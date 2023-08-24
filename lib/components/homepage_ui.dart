77165fe2e30afa84df00e2577932bdd757471183
Deepak16080 <dy16080@gmail.com>
2023-08-24 12:40:57 +0530

    Add: Shape in home screen

--- lib/components/homepage_ui.dart
+++ lib/components/homepage_ui.dart
@@ -13,14 +13,14 @@ class _HomepageUiState extends State<HomepageUi> {
     return Scaffold(
         body: Column(
       children: [
-        Container(
-          height: 100,
-          width: 200,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          alignment: Alignment.center,
-          child: Image.asset('assets/images/Timer.png', fit: BoxFit.cover),
+        CustomPaint(
+          size: Size(
+              400,
+              (400 * 0.625)
+                  .toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+          painter: RPSCustomPainter(),
+
+          child: SizedBox(height: 200, width: 400, child: Text("Container")),
         ),
         Center(
           child: Row(
@@ -29,17 +29,13 @@ class _HomepageUiState extends State<HomepageUi> {
               Container(
                 height: 100,
                 width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(20),
-                ),
                 alignment: Alignment.center,
               ),
               Container(
                 height: 100,
                 width: 100, // 200
                 decoration: BoxDecoration(
-                  color: Colors.red,
+                  color: Colors.green,
                   borderRadius: BorderRadius.circular(20),
                 ),
                 alignment: Alignment.center,
@@ -49,5 +45,60 @@ class _HomepageUiState extends State<HomepageUi> {
         ),
       ],
     ));
+  }
+}
+
+// class FirstConClipper extends CustomClipper<Path> {
+//   @override
+//   Path getClip(Size size) {
+//     Path path = Path();
+//     path.moveTo(0, size.height);
+//     path.lineTo(size.height - 20, 0);
+
+//     path.lineTo(0, size.height);
+//     path.lineTo(size.height, 0);
+//     return path;
+//   }
+
+//   @override
+//   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+// }
+class RPSCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Square
+
+    Paint paintFill1 = Paint()
+      ..color = const Color.fromARGB(255, 37, 150, 190)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = size.width * 0.00
+      ..strokeCap = StrokeCap.butt
+      ..strokeJoin = StrokeJoin.miter;
+
+    Path path_1 = Path();
+    path_1.moveTo(size.width * 0.3781292, size.height * 0.0920000);
+    path_1.lineTo(size.width * 0.9400102, size.height * 0.1007763);
+    path_1.lineTo(size.width * 0.9400102, size.height * 0.3552884);
+    path_1.lineTo(size.width * 0.3762500, size.height * 0.3991698);
+    path_1.lineTo(size.width * 0.3781292, size.height * 0.0920000);
+    path_1.close();
+
+    canvas.drawPath(path_1, paintFill1);
+
+    // Square
+
+    Paint paintStroke1 = Paint()
+      ..color = const Color.fromARGB(255, 37, 150, 190)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 0
+      ..strokeCap = StrokeCap.butt
+      ..strokeJoin = StrokeJoin.miter;
+
+    canvas.drawPath(path_1, paintStroke1);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
