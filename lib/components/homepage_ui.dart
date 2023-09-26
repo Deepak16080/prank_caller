f74c94878571f85a1ef7d3c2c2cb7a134d871abb
Deepak16080 <dy16080@gmail.com>
2023-09-26 13:35:05 +0530

    fix: homepageui fixedd

--- lib/components/homepage_ui.dart
+++ lib/components/homepage_ui.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_picker/picker.dart';
 import 'package:prank_caller/components/caller_ui_page.dart';
+import 'package:prank_caller/components/caller_voice.dart';
 import 'package:prank_caller/components/contact_page.dart';
+import 'package:prank_caller/components/ringtone.dart';
 import 'package:prank_caller/models/ringtone_model_list.dart';
 import 'package:prank_caller/utils/enums.dart';
 import 'package:prank_caller/widget/app_text.dart';
@@ -79,22 +82,23 @@ class _HomepageUiState extends State<HomepageUi> {
                     width: MediaQuery.of(context).size.width * 0.95,
                     decoration: BoxDecoration(
                       shape: BoxShape.rectangle,
-                      color: Colors.purple[100],
+                      color: Colors.indigo[200],
                       borderRadius: BorderRadius.circular(5),
                     ),
                     child: Column(
-                      children: const [
+                      children: [
                         SizedBox(
                           height: 20,
                         ),
                         Row(
                           children: [
                             SizedBox(
-                              width: 80,
+                              width: 50,
                             ),
                             CircleAvatar(
                               radius: 30,
-                              backgroundImage: AssetImage('assets/Profile/Profile2.png'),
+                              backgroundColor: Colors.transparent,
+                              backgroundImage: AssetImage('assets/images/timer.png'),
                             ),
                             SizedBox(
                               width: 20,
@@ -102,17 +106,27 @@ class _HomepageUiState extends State<HomepageUi> {
                             Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                AppText(
-                                  'Timer',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.white,
+                                InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      onTap();
+                                    });
+                                  },
+                                  child: AppText(
+                                    'Timer',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                    ),
                                   ),
                                 ),
                                 SizedBox(
                                   height: 8,
                                 ),
-                                Text('its a timer')
+                                Text(
+                                  " ${selectedDuration?.inMinutes ?? 0} minutes ${selectedDuration?.inSeconds ?? 0} seconds",
+                                  style: TextStyle(color: Colors.white),
+                                )
                               ],
                             ),
                           ],
@@ -132,27 +146,43 @@ class _HomepageUiState extends State<HomepageUi> {
                         width: MediaQuery.of(context).size.width * 0.45,
                         decoration: BoxDecoration(
                           shape: BoxShape.rectangle,
-                          color: Colors.purple[100],
+                          color: Colors.red[200],
                           borderRadius: BorderRadius.circular(5),
                         ),
                         child: Center(
                           child: Column(
-                            children: const [
+                            children: [
                               SizedBox(
                                 height: 40,
                               ),
                               CircleAvatar(
                                 radius: 30,
-                                backgroundImage: AssetImage('assets/Profile/Profile2.png'),
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: AssetImage('assets/images/callervoice.png'),
                               ),
                               SizedBox(
                                 height: 15,
                               ),
-                              AppText('CallerVoice',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.white,
-                                  )),
+                              InkWell(
+                                onTap: () async {
+                                  final audio = await Navigator.push(
+                                      context, MaterialPageRoute(builder: (context) => const CallerVoice()));
+                                  if (audio is AppAudio) {
+                                    selectedAudio = audio;
+                                  }
+                                },
+                                child: AppText('CallerVoice',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                    )),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              if (selectedAudio != null)
+                                Text(' ${selectedAudio?.name ?? 'No audio selected'}',
+                                    style: TextStyle(color: Colors.white)),
                             ],
                           ),
                         ),
@@ -162,27 +192,51 @@ class _HomepageUiState extends State<HomepageUi> {
                         width: MediaQuery.of(context).size.width * 0.45,
                         decoration: BoxDecoration(
                           shape: BoxShape.rectangle,
-                          color: Colors.purple[100],
+                          color: Colors.orange[300],
                           borderRadius: BorderRadius.circular(5),
                         ),
                         child: Center(
                           child: Column(
-                            children: const [
+                            children: [
                               SizedBox(
                                 height: 40,
                               ),
                               CircleAvatar(
                                 radius: 30,
-                                backgroundImage: AssetImage('assets/Profile/Profile2.png'),
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: AssetImage('assets/images/ringtone.png'),
                               ),
                               SizedBox(
                                 height: 15,
                               ),
-                              AppText('Ringtones',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.white,
-                                  )),
+                              InkWell(
+                                onTap: () async {
+                                  final audio = await Navigator.push(
+                                      context, MaterialPageRoute(builder: (context) => const Ringtone()));
+                                  if (audio is AppAudio) {
+                                    selectedAudio = audio;
+                                  }
+                                  setState(() {});
+                                },
+                                child: AppText('Ringtones',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                    )),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              if (selectedAudio != null)
+                                GestureDetector(
+                                  onTap: () {
+                                    selectedAudio?.play();
+                                  },
+                                  child: Text(
+                                    "${selectedAudio?.name?.replaceAll("", "").replaceAll(".mp3", "")}",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
                             ],
                           ),
                         ),
@@ -212,6 +266,14 @@ class _HomepageUiState extends State<HomepageUi> {
                           Row(
                             children: [
                               SizedBox(
+                                width: 20,
+                              ),
+                              CircleAvatar(
+                                radius: 20,
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: AssetImage('assets/images/contact.png'),
+                              ),
+                              SizedBox(
                                 width: 10,
                               ),
                               Padding(
@@ -228,14 +290,16 @@ class _HomepageUiState extends State<HomepageUi> {
                               if (selectedContact != null)
                                 Text(
                                   " ${selectedContact?.displayName}",
-                                  style: TextStyle(color: Colors.white, fontSize: 10),
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
                                 ),
                             ],
                           ),
                         ])),
                   ),
                   SizedBox(
-                    height: 50,
+                    height: 80,
                   ),
                   MaterialButton(
                       shape: RoundedRectangleBorder(
@@ -243,8 +307,7 @@ class _HomepageUiState extends State<HomepageUi> {
                           side: BorderSide(color: Colors.black, width: 1, style: BorderStyle.solid)),
                       onPressed: () {
                         if (selectedContact == null) {
-                          toast(context, "Please select a contact first");
-                          return;
+                          return toast(context, "Please select a contact first");
                         }
                         Navigator.push(
                             context,
@@ -253,6 +316,7 @@ class _HomepageUiState extends State<HomepageUi> {
                                       contact: selectedContact!,
                                     )));
                       },
+                      color: Colors.white,
                       splashColor: Colors.blueAccent,
                       textColor: Colors.green,
                       child: Text('Call Me')),
@@ -261,6 +325,62 @@ class _HomepageUiState extends State<HomepageUi> {
             ),
           ]),
         ));
+  }
+
+  onTap() {
+    Picker(
+      adapter: NumberPickerAdapter(data: <NumberPickerColumn>[
+        const NumberPickerColumn(begin: 0, end: 60, suffix: Text(' minutes'), jump: 0),
+        const NumberPickerColumn(begin: 0, end: 60, suffix: Text('seconds'), jump: 5)
+      ]),
+      delimiter: <PickerDelimiter>[
+        PickerDelimiter(
+          child: Container(
+            //          decoration: const BoxDecoration(
+            //   color: Colors.white,
+            //   borderRadius: BorderRadius.all(Radius.circular(10)),
+            //   boxShadow: [
+            //     BoxShadow(
+            //       color: Colors.grey,
+            //       blurRadius: 20.0, // Soften the shaodw
+            //       spreadRadius: 2.0,
+            //       offset: Offset(0.0, 0.0),
+            //     )
+            //   ],
+            // ),
+            width: 30.0,
+            alignment: Alignment.center,
+            child: const Icon(Icons.more_vert),
+          ),
+        )
+      ],
+      confirmText: 'OK',
+      onSelect: (picker, index, selected) {
+        if (index == 0) {
+          return toast(context, 'Please select a time');
+        }
+        print(selected.toString());
+        // print a time in text box when selected
+      },
+      onCancel: () {
+        debugPrint('cancel');
+      },
+      confirmTextStyle: const TextStyle(inherit: false, color: Colors.red, fontSize: 22),
+      title: const Text('Select Time'),
+      hideHeader: true,
+      height: 200.0,
+      backgroundColor: Colors.white,
+      selectedTextStyle: TextStyle(color: Colors.blue),
+      onConfirm: (Picker picker, List<int> value) {
+        // You get your duration here
+
+        Duration resultingDuration = Duration(minutes: value[1], seconds: value[1]);
+        // Duration(minutes: 0, seconds: 0) is the default value
+        selectedDuration = resultingDuration;
+
+        setState(() {});
+      },
+    ).showDialog(context);
   }
 
   void toast(BuildContext context, String text) {
